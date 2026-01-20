2ea407a0a84f1a5bdbbf5bb0f9a3cbfac0c124b4
joan0343 <sr4500431@gmail.com>
2026-01-20 15:57:32 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+// Tinopal UV v2 - Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("tinopal-cache-v2").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
